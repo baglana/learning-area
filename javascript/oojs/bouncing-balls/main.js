27a8187b9739788af0006174ca9f640395b4bbed
baglana <102351104+baglana@users.noreply.github.com>
2024-10-09 14:10:09 +0500

    Do oojs bouncing-balls assessment

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -1,5 +1,8 @@
 // setup canvas
 
+import { Ball } from "./ball.js";
+import { EvilCircle } from "./evilCircle.js";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -17,3 +20,54 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+const scorePara = document.querySelector("p");
+
+const evilCircle = new EvilCircle(
+  random(0 + EvilCircle.size, width - EvilCircle.size),
+  random(0 + EvilCircle.size, width - EvilCircle.size)
+);
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    if (!ball.exists) {
+      continue;
+    }
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+    
+  scorePara.textContent = `Ball count: ${Ball.count}`;
+
+  requestAnimationFrame(loop);
+}
+
+loop();
+
+export { ctx, width, height, balls, randomRGB };
